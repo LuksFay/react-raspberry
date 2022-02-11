c6d45ac4d143b64ab3c33780b8a7a6be670642cc
luksfay <lukspicco@hotmail.com>
2022-02-11 06:56:01 +0000

    margin top

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const About = () =>{
 	return(
 		<>
-			<div clasName='flex items-center justify-center h-screen'>
-				<div className='bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10'>
+			<div clasName='flex items-center justify-center h-screen mt-10'>
+				<div className='bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10 text-center'>
 					About
 				</div>
 			</div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Home = () =>{
 	return(
 		<>
-			<div clasName='flex items-center justify-center h-screen'>
-				<div className='bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10'>
+			<div clasName='flex items-center justify-center h-screen mt-10'>
+				<div className='bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10 text-center'>
 					Home
 				</div>
 			</div>
