8af67bb68181e14fec0d87c38729fd4fb5b3db37
luksfay <lukspicco@hotmail.com>
2022-02-11 02:55:13 +0000

    cerrando los Route

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom';
 import {
 	BrowserRouter,
 	Route,
-	Routes,
-	Redirect,
-	Switch
+	Routes
 	} from 'react-router-dom';
 	import About from './pages/About/About';
 	import Home from './pages/Home/Home';
@@ -17,12 +15,8 @@ function App() {
     <BrowserRouter>
     	<main>
     		<Routes>
-    			<Route path="/about" element{<About />}>
-    				
-    			</Route>
-    			<Route path="/" element{<Home />}>
-    				
-    			</Route>
+    			<Route path="/about" element{<About />} />
+    			<Route path="/" element{<Home />} />
     		</Routes>
     	</main>
     </BrowserRouter>
