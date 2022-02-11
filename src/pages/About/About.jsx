66b29b5f45b05a2e216ab0681d3565f2c0c6088b
luksfay <lukspicco@hotmail.com>
2022-02-11 06:46:42 +0000

    mejorando el estilo de las pages

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const About = () =>{
 	return(
 		<>
-			<div>
-				<h1>
+			<div clasName='flex items-center justify-center h-screen'>
+				<div className='bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10'>
 					About
-				</h1>
+				</div>
 			</div>
 		</>
 	)
